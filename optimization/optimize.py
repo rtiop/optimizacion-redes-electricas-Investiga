7a3d0890441da04f39896ccf4baae5e1655f895e
rtiop <eparedes@colegiosil.com>
2024-03-09 10:17:44 +0100

    Added exception raising for impossible target value

--- optimization/optimize.py
+++ optimization/optimize.py
@@ -9,12 +9,25 @@ with open(fileName, 'r') as file:
 
 
 # Data is a list of dictionaries each of which is one power plant
+
+# Change data type of all data to integer    
+for station in data:
+    for x in station:
+        station [x] = float(station[x])
+
+
 # Prompt user for target power production
 target = int(input("Target power production in kWh (not bigger than the total production capacity of the set): "))
 max_capacity = 0
 for plant in data:
     max_capacity += plant["producción"]
 
+# Check if the target is in the possible range
+if target > max_capacity:
+    raise ValueError("Target above max production capacity.")
+
+
+# Create initial
 # Hill-climbing: 
 #   Consider all neighbours
 #   Calculate the cost of all of them
